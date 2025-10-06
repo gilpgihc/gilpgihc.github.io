7a60a21ca75cea6cc96e16e79ed8f9a267491172
gilpgdm <gilpgdm@gmail.com>
2025-10-06 00:15:59 -0600

    sw 5.0

--- docs/sw.js
+++ docs/sw.js
@@ -1,7 +1,7 @@
 /* Espera 11 minutos después de hacer los cambios en tu sitio, para depués
  * actualizar este archivo. */
 
-const VERSION = "4.22"
+const VERSION = "5.0"
 
 const CACHE = "cache"
 
